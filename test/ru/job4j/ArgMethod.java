8403d1b828d54e58216b63145ff2c08e6c966b90
DNS <shedrin79@gmail.com>
2020-12-24 14:07:52 +0400

    hello

---
+++ test/ru/job4j/ArgMethod.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class ArgMethod {
+    public static void hello (String name){
+        System.out.println ("Привет" + name);
+            }
+            public static void main (String[]args){
+        String name = "Петр";
+        ArgMethod.hello (name);
+            }
+
+}
