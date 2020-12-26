224bee4f0bd6dab7ee36e0b91a3d101579f31a2c
DNS <shedrin79@gmail.com>
2020-12-26 17:14:50 +0400

    Converter

---
+++ test/ru/job4j/Converter.java
@@ -0,0 +1,19 @@
+package ru.job4j;
+
+public class Converter {
+    public static int rubleToEuro (int value){
+        int result = ( value / 70);
+        return result;
+            }
+            public static int rubleToDollar (int value){
+        int result = (value / 60);
+        return result;
+            }
+            public static void main (String [] args){
+        int euro = Converter.rubleToEuro (140);
+        int dollar = Converter.rubleToDollar (120);
+        System.out.println ("140 рублей - это" + euro + "евро" );
+        System.out.println ("120 рублей - это" + dollar + "доллар");
+            }
+
+}
