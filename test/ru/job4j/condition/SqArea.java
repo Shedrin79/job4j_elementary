881d529802c586138e76403424d4cc04688646fb
DNS <shedrin79@gmail.com>
2020-12-28 14:46:42 +0300

    Площадь квадрата2

--- test/ru/job4j/condition/SqArea.java
+++ test/ru/job4j/condition/SqArea.java
@@ -2,17 +2,15 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square ( double p, double k ){
-        double first = k + 1;
-        double second = 2 * first;
-        double h = ( p / second);
+        double h = ( p / (2 * ( k + 1)));
         double l = h * k;
         double s = l * h;
         return s;
     }
 
     public static void main (String [] args){
-        double s = SqArea.square ( 6, 2 );
-        System.out.println (" если периметр = 6, а коэффициент k = 2, то площадь равна " + s);
+        double result = SqArea.square ( 6, 2 );
+        System.out.println (" если периметр = 6, а коэффициент k = 2, то площадь равна " + result);
 
     }
 }
