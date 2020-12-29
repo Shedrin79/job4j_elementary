644dde6ef7091a6cca9b6bc754116ec6c17c880b
DNS <shedrin79@gmail.com>
2020-12-29 11:09:41 +0300

    SERG

--- test/ru/job4j/spot/Out.java
+++ test/ru/job4j/spot/Out.java
@@ -1,11 +1,11 @@
 package ru.job4j.spot;
 
 public class Out {
-    public static void main (String[]args){
+    public static void main(String[] args) {
         String idea = "Я люблю Java!";
         String status = "Но я новичок ";
         int year = 2020;
         String result = idea + status + year + " год. С Новым Годом!";
-        System.out.println (result);
+        System.out.println(result);
     }
 }
