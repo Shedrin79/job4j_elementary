603391ab2512122f383c43dfb01069bce09159bf
DNS <shedrin79@gmail.com>
2021-01-07 13:34:44 +0300

    Club

--- test/ru/job4j/condition/Club.java
+++ test/ru/job4j/condition/Club.java
@@ -5,14 +5,15 @@ public class Club {
         if (hasmany || ourfriend) {
             System.out.println("я могу пойти в клуб");
         } else {
-            System.out.println("я не могу пойти в клуб");
+            System.out.println("я НЕ могу пойти в клуб");
         }
     }
 
     public static void main(String[] args) {
         permission(true, true);
         permission(true, false);
-        permission(false, true);
         permission(false, false);
+        permission(false, true);
+
     }
 }
---
+++ test/ru/job4j/condition/LogicNot.java
@@ -0,0 +1,44 @@
+package ru.job4j.condition;
+
+public class LogicNot {
+
+    public static boolean isPositive(int num) {
+        return num > 0;
+    }
+
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    public static boolean notEven(int num) {
+        return !isEven(num);
+    }
+
+    public static boolean notPositive(int num) {
+        return !isPositive(num);
+    }
+
+    public static boolean notEvenAndPositive(int num) {
+        return (!isEven(num) && isPositive(num));
+    }
+
+    public static boolean evenOrNotPositive(int num) {
+        return isPositive(num);
+    }
+
+    public static void main(String[] args) {
+        boolean rsl = LogicNot.isPositive(1);
+        System.out.println(rsl);
+        boolean rsl2 = LogicNot.isEven(6);
+        System.out.println(rsl2);
+        boolean rsl3 = LogicNot.notEven(8);
+        System.out.println(rsl3);
+        boolean rsl4 = LogicNot.notPositive(-8);
+        System.out.println(rsl4);
+        boolean rsl5 = LogicNot.notEvenAndPositive(9);
+        System.out.println(rsl5);
+        boolean rsl6 = LogicNot.evenOrNotPositive(10);
+        System.out.println(rsl6);
+    }
+
+}
