c5ea3c5f2bedfd66305d7c9f9cfaff8ef412d6af
DNS <shedrin79@gmail.com>
2021-01-11 22:37:36 +0300

    CounterTest

--- test/ru/job4j/condition/Proverka.java
+++ test/ru/job4j/condition/Proverka.java
@@ -1,6 +1,7 @@
 package ru.job4j.condition;
 
-public class Counter {
+public class Proverka {
+
     public static int sum(int start, int finish) {
         int sum = 0;
         for (int index = start; index <= finish; index++) {
@@ -23,10 +24,12 @@ public class Counter {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
+        System.out.println("ПЕРЕРЫВ");
 
         System.out.println(sumByEven(0, 10));
         System.out.println(sumByEven(3, 8));
         System.out.println(sumByEven(1, 1));
+
     }
 }
 
