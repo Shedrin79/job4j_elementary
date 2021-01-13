13e2580f5d8fa9031239e4ca7a56bf6d6572677d
DNS <shedrin79@gmail.com>
2021-01-13 07:34:35 +0300

    FactorialTest

--- test/ru/job4j/condition/FactorialTest.java
+++ test/ru/job4j/condition/FactorialTest.java
@@ -9,14 +9,14 @@ public class FactorialTest {
 
     @Test
     public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
-        int result = Factorial.calc(1, 5);
+        int result = Factorial.calc(5);
         int expected = 120;
         assertThat(result, is(expected));
     }
 
     @Test
     public void whenCalculateFactorialForZeroThenOne() {
-        int result = Factorial.calc(0, 0);
+        int result = Factorial.calc(0);
         int expected = 1;
         assertThat(result, is(expected));
     }
