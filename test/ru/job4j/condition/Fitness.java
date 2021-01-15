552b6f43817782055b318f7fa72d18342b8edee2
DNS <shedrin79@gmail.com>
2021-01-15 16:45:19 +0300

    Fitness

--- test/ru/job4j/condition/Fitness.java
+++ test/ru/job4j/condition/Fitness.java
@@ -2,16 +2,13 @@ package ru.job4j.condition;
 
 public class Fitness {
     public static int calc(int ivan, int nik) {
-        if (ivan < nik) {
-            int month = 1;
-            while ((ivan *= 3) < (nik *= 2)) {
-                month = month + 1;
-            }
-            return month;
-        } else {
-            return 0;
+        int month = 0;
+        while ((ivan = ivan *= 3) < (nik = nik *= 2)) {
+            month = month + 1;
         }
+        return month;
     }
+
 
     public static void main(String[] args) {
         int rsl = Fitness.calc(50, 100);
