d2fac01bdc1b11988d7a26ddd7212b1e3725265c
DNS <shedrin79@gmail.com>
2021-01-18 15:59:56 +0300

    MortgageTest

--- test/ru/job4j/condition/Mortgage.java
+++ test/ru/job4j/condition/Mortgage.java
@@ -2,19 +2,19 @@ package ru.job4j.condition;
 
 public class Mortgage {
 
-    public static double year(double amount, double salary, double percent) {
+    public static int year(double amount, double salary, double percent) {
 
         int age = 0;
-        double ostatok = amount + amount * percent;
-        while (ostatok > 0) {
-            ostatok = ostatok - salary;
+        while (amount > 0) {
+            amount = amount + (amount * percent / 100);
+            amount = amount - salary;
             age = age + 1;
         }
         return age;
     }
 
     public static void main(String[] args) {
-        System.out.println(year(100, 70, 0.5));
+        System.out.println(year(1000, 1200, 1));
 
     }
 }
--- test/ru/job4j/condition/MortgageTest.java
+++ test/ru/job4j/condition/MortgageTest.java
@@ -1,7 +1,6 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
-
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -9,13 +8,13 @@ public class MortgageTest {
 
     @Test
     public void when1year() {
-        double year = Mortgage.year(1000, 1200, 1);
+        int year = Mortgage.year(1000, 1200, 1);
         assertThat(year, is(1));
     }
 
     @Test
     public void when2Year() {
-        double year = Mortgage.year(100, 120, 50);
+        int year = Mortgage.year(100, 120, 50);
         assertThat(year, is(2));
     }
 
