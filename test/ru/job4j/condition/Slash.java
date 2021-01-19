a231c5bbd58955ddf5ce2adca17345793f80e697
DNS <shedrin79@gmail.com>
2021-01-19 22:29:28 +0300

    Slash

--- test/ru/job4j/condition/Slash.java
+++ test/ru/job4j/condition/Slash.java
@@ -4,19 +4,9 @@ public class Slash {
 
     public static void draw(int size) {
 
-        for (int row = size; row >= 0; row--) {
-            for (int cell = size; cell >= 0; cell--) {
-                if (row + cell == size - 1) {
-                    System.out.print("X");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
-                if (row == cell) {
+                if ((row == cell) || (row + cell == size - 1)) {
                     System.out.print("X");
                 } else {
                     System.out.print(" ");
@@ -28,7 +18,7 @@ public class Slash {
     }
 
     public static void main(String[] args) {
-        System.out.println("Draw by 5");
+        System.out.println("Draw by 3");
         draw(3);
         System.out.println("Draw by 5");
         draw(5);
