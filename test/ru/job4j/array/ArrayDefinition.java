9bd563adf725a5a139139ba83237a67fad248bed
DNS <shedrin79@gmail.com>
2021-01-20 23:31:09 +0300

    ArrayDefinition

--- test/ru/job4j/array/ArrayDefinition.java
+++ test/ru/job4j/array/ArrayDefinition.java
@@ -3,7 +3,10 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] age = new short[10];
+        System.out.println("размер массива short равен:" + 10);
         String[] lastname = new String[100500];
+        System.out.println("размер массива String равен:" + 100500);
         float[] price = new float[40];
+        System.out.println("размер массива float равен :" + 40);
     }
 }
