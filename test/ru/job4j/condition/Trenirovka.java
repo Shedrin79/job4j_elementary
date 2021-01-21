4946f75d44c10690933dd486ecb47b4f634b482c
DNS <shedrin79@gmail.com>
2021-01-21 21:25:06 +0300

    ArrayLoop

--- test/ru/job4j/condition/Trenirovka.java
+++ test/ru/job4j/condition/Trenirovka.java
@@ -3,12 +3,24 @@ package ru.job4j.condition;
 public class Trenirovka {
 
     public static void main(String[] args) {
-        int num = -1;
-        boolean result = num > 0;
-        boolean result2 = num < 0;
-        System.out.println(result);
-        System.out.println(result2);
+
+        int[] numbers = new int[10];
+        numbers[0] = 0;
+        numbers[1] = 1;
+        numbers[2] = 2;
+        numbers[3] = 3;
+        numbers[4] = 4;
+        numbers[5] = 5;
+        numbers[6] = 6;
+        numbers[7] = 7;
+        numbers[8] = 8;
+        numbers[9] = 9;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(numbers[i]);
+        }
     }
 }
+
+
 
 
