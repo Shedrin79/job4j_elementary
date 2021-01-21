0ea8739f0980875349ad142b3c7f393377888b1e
DNS <shedrin79@gmail.com>
2021-01-21 15:50:56 +0300

    ArrayDefinition

--- test/ru/job4j/array/ArrayDefinition.java
+++ test/ru/job4j/array/ArrayDefinition.java
@@ -2,11 +2,18 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] age = new short[10];
-        System.out.println("размер массива short равен:" + 10);
-        String[] lastname = new String[100500];
-        System.out.println("размер массива String равен:" + 100500);
-        float[] price = new float[40];
-        System.out.println("размер массива float равен :" + 40);
+
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Petr First";
+        names[2] = "Petr Second";
+        names[3] = "Petr Third";
+        System.out.println(names[0]);
+        String name = names[1];
+        System.out.println(name);
+        System.out.println(names[2]);
+        String name1 = names[3];
+        System.out.println(name1);
+
     }
 }
---
+++ test/ru/job4j/condition/Trenirovka.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Trenirovka {
+
+    public static void main(String[] args) {
+        int num = -1;
+        boolean result = num > 0;
+        boolean result2 = num < 0;
+        System.out.println(result);
+        System.out.println(result2);
+    }
+}
+
+
