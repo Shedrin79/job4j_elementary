74b2159e66dd70f32f3404135ce5e4b099b8997d
DNS <shedrin79@gmail.com>
2021-01-23 09:42:18 +0300

    Trenirovka

--- test/ru/job4j/condition/Trenirovka.java
+++ test/ru/job4j/condition/Trenirovka.java
@@ -3,13 +3,10 @@ package ru.job4j.condition;
 public class Trenirovka {
 
     public static void main(String[] args) {
-        String[] numbers = new String[5];
-        for (int index = 0; index < numbers.length; index++) {
-            System.out.println(numbers[index]);
+        String[] names = {"Света", "Лена", "Вика", "Вероника"};
+        for (int i = 0; i < names.length; i++) {
+            names[i] = names + "  Привет!!!";
+            System.out.println(names[i]);
         }
     }
 }
-
-
-
-
