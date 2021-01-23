118899cf2264c4a526240a415ccc9eb2071320ae
DNS <shedrin79@gmail.com>
2021-01-23 22:19:17 +0300

    Square

--- test/ru/job4j/array/Square.java
+++ test/ru/job4j/array/Square.java
@@ -4,15 +4,16 @@ public class Square {
     public static int[] calculate(int bound) {
         int[] rst = new int[bound];
         for (int i = 0; i < rst.length; i++) {
-        rst[i] = i * i;
+            rst[i] = i * i;
         }
         return rst;
     }
 
     public static void main(String[] args) {
-        int[] array = calculate(4);
+        int[] array = calculate(5);
         for (int index = 0; index < array.length; index++) {
             System.out.println(array[index]);
         }
     }
 }
+
