bff0fb4e41c9c18b5dc3561ff357dad2278d5d7a
DNS <shedrin79@gmail.com>
2021-01-24 17:17:52 +0300

    FindLoopTest

---
+++ test/ru/job4j/array/FindLoop.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class FindLoop {
+
+    public static int indexOf(int[] data, int el) {
+        int rst = -1;
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+}
+
+
+
+
+
--- test/ru/job4j/condition/Trenirovka.java
+++ test/ru/job4j/condition/Trenirovka.java
@@ -3,10 +3,10 @@ package ru.job4j.condition;
 public class Trenirovka {
 
     public static void main(String[] args) {
-        String[] names = {"Света   ", "Лена    ", "Вика    ", "Вероника"};
-        for (int i = 0; i < names.length; i++) {
-            names[i] = names[i] + "  Привет!!!";
-            System.out.println(names[i]);
+        int[] numbers = new int[10];
+        for (int index = 0; index < numbers.length; index++) {
+            numbers[index] = index;
+            System.out.println(numbers[index]);
         }
     }
-}
+    }
