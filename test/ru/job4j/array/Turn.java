09322745c1cd96bb797ae76d63e09dd0fea58746
DNS <shedrin79@gmail.com>
2021-01-27 10:24:35 +0300

    TurnTest

--- test/ru/job4j/array/Turn.java
+++ test/ru/job4j/array/Turn.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class Turn {
     public static int[] back(int[] array) {
-        for (int i = 0; i < array.length - i; i++) {
+        for (int i = 0; i < array.length / 2; i++) {
             int temp = array[i];
             array[i] = array[array.length - 1 - i];
             array[array.length - 1 - i] = temp;
@@ -11,7 +11,7 @@ public class Turn {
     }
 
     public static void main(String[] args) {
-        int[] nuts = new int[]{3, 4, 7, 11, 1000};
+        int[] nuts = new int[]{3, 4, 7, 11, 1000, 2000};
         int[] rsl = back(nuts);
         for (int i = 0; i < rsl.length; i++) {
             System.out.println(rsl[i]);
