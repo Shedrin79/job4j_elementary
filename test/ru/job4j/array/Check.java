0981acc63c2ddc2b9adc52f882b58f6daf2e4185
schedrin_s <shedrin79@gmail.com>
2021-01-28 09:59:22 +0400

    Check

---
+++ test/ru/job4j/array/Check.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+public class Check {
+
+    public static boolean mono(boolean[] data) {
+        boolean result = true;
+        for (int index = 0; index < data.length - 1; index++) {
+            if (data[0] = data[index + 1]) {
+                return result;
+            } else {
+                return false;
+            }
+        }
+        return result;
+    }
+}
+
+
+
+
+
