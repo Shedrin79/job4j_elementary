3125159aff38251bed5df495cfdf018f59e544e2
schedrin_s <shedrin79@gmail.com>
2021-01-30 11:33:57 +0400

    EqLast

--- src/ru/job4j/array/EqLast.java
+++ src/ru/job4j/array/EqLast.java
@@ -3,15 +3,12 @@ package ru.job4j.array;
 public class EqLast {
 
     public static boolean check(int[] left, int[] right) {
-        boolean rsl = false;
-        if (left[left.length - 1] == right[right.length - 1]){
-            rsl = true;
-        }
-        return rsl;
-}
+        return left[left.length - 1] == right[right.length - 1];
+    }
+
 
     public static void main(String[] args) {
-        int[] leftt = new int[]{1, 2, 3, 4, 0, 4};
+        int[] leftt = new int[]{1, 2, 3, 4, 0, 4, 3};
         int[] rightt = new int[]{1, 2, 3, 4, 5, 5, 4};
         System.out.println(check(leftt, rightt));
     }
--- src/ru/job4j/condition/Trenirovka.java
+++ src/ru/job4j/condition/Trenirovka.java
@@ -2,12 +2,13 @@ package ru.job4j.condition;
 
 public class Trenirovka {
 
-    public static void main(String[] args) {
-        int[] numbers = new int[10];
-        for (int index = 0; index < numbers.length; index++) {
-            numbers[index] = index;
-            System.out.println(numbers[index]);
+    public static boolean check(int[] left, int[] right) {
+        return left[left.length - 1] == right[right.length - 1];
+    }
 
-        }
+    public static void main(String[] args) {
+        int[] leftt = new int[]{1, 2, 3, 4, 0, 4, 5};
+        int[] rightt = new int[]{1, 2, 3, 4, 5, 5, 4};
+        System.out.println(check(leftt, rightt));
     }
-    }
+}
