b0a190adad58ddc8f7ff9628349fead1fc068db4
schedrin_s <shedrin79@gmail.com>
2021-01-30 21:50:11 +0300

    MinDiapason

---
+++ src/ru/job4j/array/MinDiapason.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class MinDiapason {
+    public static int findMin(int[] array, int start, int finish) {
+        int min = array[start];
+        for (int index = start; index <= finish - 1; index ++) {
+            if (array[index + 1] < array[index]) {
+                min = array[index + 1];
+            }
+        }
+        return min;
+        }
+
+    public static void main(String[] args) {
+        int[]mass = new int[]{7, 4, 5, 2, 8, 16, 27, 2, 1};
+        System.out.println(findMin(mass, 7, 8));
+    }
+}
