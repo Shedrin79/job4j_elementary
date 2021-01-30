1e4c6e688dc514af11ee4004b87656aee99de0bf
schedrin_s <shedrin79@gmail.com>
2021-01-30 22:48:47 +0300

    Min

--- src/ru/job4j/array/Min.java
+++ src/ru/job4j/array/Min.java
@@ -3,16 +3,16 @@ package ru.job4j.array;
 public class Min {
     public static int findMin(int[] array) {
         int min = array[0];
-        for (int index = 0; index < array.length - 1; index++) {
-            if (array[index + 1] < min) {
-                min = array[index + 1];
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] < min) {
+                min = array[index];
             }
         }
         return min;
     }
 
     public static void main(String[] args) {
-        int[] mass = new int[]{6, 2, 3, 4, 5, 6, 1, 9, 0};
+        int[] mass = new int[]{6, 2, 3, 4, 5, 6, 1, 9};
         System.out.println(findMin(mass));
     }
 }
