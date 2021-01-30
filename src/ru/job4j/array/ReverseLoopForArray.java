3bf0c43dfa1f98f76c9c92b7dbfac618643c6dbd
schedrin_s <shedrin79@gmail.com>
2021-01-30 11:27:49 +0400

    ReverseLoopForArray

--- src/ru/job4j/array/ReverseLoopForArray.java
+++ src/ru/job4j/array/ReverseLoopForArray.java
@@ -2,17 +2,14 @@ package ru.job4j.array;
 
 public class ReverseLoopForArray {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 7};
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 7, 3, 9};
         for (int i = 0; i < numbers.length; i++) {
-            if (i % 2 == 0 && numbers.length % 2 != 0) {
+            if ((numbers.length - 1 - i) % 2 == 0) {
                 System.out.println("Текущий элемент массива c четным индексом: " + numbers[numbers.length - 1 - i]
                 );
-            } else {
-                if (i % 2 == 0 && numbers.length % 2 == 0) {
-                    System.out.println("Текущий элемент массива c четным индексом: " + numbers[numbers.length - 2 - i]);
-                }
+            }
             }
         }
     }
-}
+
 
