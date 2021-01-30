87d37adb901212db01a867d7fd4e5322afe01d9e
schedrin_s <shedrin79@gmail.com>
2021-01-30 17:21:21 +0400

    FindLoop

--- src/ru/job4j/array/FindLoopTest.java
+++ src/ru/job4j/array/FindLoopTest.java
@@ -52,13 +52,29 @@ public class FindLoopTest {
     }
 
     @Test
-    public void whenFind3() {
+    public void whenFind10() {
         int[] input = new int[] {5, 2, 10, 2, 4};
         int value = 10;
         int start = 2;
         int finish = 4;
         int result = FindLoop.indexOf(input, value, start, finish);
         int expect = 2;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenFind8() {
+        int[] input = new int[] {5, 10, 3};
+        int value = 8;
+        int result = FindLoop.indexOf(input, value);
+        int expect = - 1;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenFind100() {
+        int[] input = new int[] {5, 1, 3, 7, 4, 100, 11, 27};
+        int value = 100;
+        int result = FindLoop.indexOf(input, value);
+        int expect = 5;
         assertThat(result, is(expect));
     }
 
