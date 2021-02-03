ae7c57c14c0204e99ca7ba8243ce92b9f7107be4
schedrin_s <shedrin79@gmail.com>
2021-02-03 21:52:22 +0300

    MatrixCheck

--- src/ru/job4j/array/Matrix.java
+++ src/ru/job4j/array/Matrix.java
@@ -4,14 +4,14 @@ public class Matrix {
 
     public static int[][] multiple(int size) {
         int[][] table = new int[size][size];
-        for (int i = 0; i < table.length; i ++) {
-            for (int j = 0; j < table[i].length; j ++) {
-                table[i][j] = (i+1) * (j+1);
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+                table[i][j] = (i + 1) * (j + 1);
             }
         }
         return table;
     }
-    }
+}
 
 
 
