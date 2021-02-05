09f10d7f61b25267387040b5b06323110553cd66
schedrin_s <shedrin79@gmail.com>
2021-02-05 21:32:40 +0300

    Defragment

--- src/ru/job4j/Defragment.java
+++ src/ru/job4j/Defragment.java
@@ -5,18 +5,20 @@ public class Defragment {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
                 int point = index;
-                        for (int i = point; i < array.length; i ++) {
-                        if (array[i] != null) {
+                for (int i = point; i < array.length; i ++) {
+                    if (array[i] != null) {
+                        int point2 = i;
                         String basa = array[point];
                         array[point] = array[i];
                         array[i] = basa;
-                        }
+                        break;
+                    }
                 }
             }
         }
         return array;
     }
-}
+    }
 
 
 
