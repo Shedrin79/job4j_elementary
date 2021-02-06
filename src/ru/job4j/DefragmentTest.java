d9bba623eeea53c69c4312e903c9b149e909f774
schedrin_s <shedrin79@gmail.com>
2021-02-06 18:17:13 +0300

    Machine

--- src/ru/job4j/DefragmentTest.java
+++ src/ru/job4j/DefragmentTest.java
@@ -27,5 +27,12 @@ public class DefragmentTest {
         String[] expected = {"I", "wanna", "be", "compressed", null, null, null};
         assertThat(compressed, is(expected));
     }
+    @Test
+    public void firstNull1() {
+        String[] input = {null, "I", "wanna", null, "be", null, "compressed", null, "again"};
+        String[] compressed = Defragment.compress(input);
+        String[] expected = {"I", "wanna", "be", "compressed", "again", null, null, null, null};
+        assertThat(compressed, is(expected));
+    }
 
 }
---
+++ src/ru/job4j/array/Machine.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+
+public class Machine {
+    public static int[] change(int money, int price) {
+        int[] coins = {10, 5, 2, 1};
+        int size = 0;
+        int[] rsl = new int[size];
+        int sdacha = money - price;
+        for (int i = 0; i < coins.length; i++) {
+            while (sdacha > coins[i]) {
+                sdacha = sdacha - coins[i];
+                size = size + 1;
+                rsl[size - 1] = coins[i];
+               }
+        }
+        return rsl;
+    }
+}
